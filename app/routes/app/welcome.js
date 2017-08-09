b2abddf0afe56ae2880c8afeebf2233e46b05060
Jacob Bare <jacob@limit0.io>
2017-08-09 09:10:47 -0500

    Consolidate transition logic

--- app/routes/app/welcome.js
+++ app/routes/app/welcome.js
@@ -4,9 +4,6 @@ const { Route } = Ember;
 
 export default Route.extend({
   actions: {
-    transitionTo(routeName) {
-      this.transitionTo(routeName);
-    },
     transitionToOrg() {
       const oid = this.get('orgManager.activeOrg.id');
       if (oid) {
