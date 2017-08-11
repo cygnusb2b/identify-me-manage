f7c9036f7e3927ead1b623d1b3d5697a75d5cc7c
Josh Worden <solocommand@gmail.com>
2017-08-11 15:56:22 -0500

    lint

--- app/components/-organization/settings/update.js
+++ app/components/-organization/settings/update.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, computed, inject: { service } } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
   isLoading: false,
