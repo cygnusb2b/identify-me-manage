a3bf245877f508cd1b6de35b8aa4aa5f94ba711d
Josh Worden <solocommand@gmail.com>
2017-08-14 15:00:02 -0500

    Cleanup unused

--- app/components/-organization/create.js
+++ app/components/-organization/create.js
@@ -1,15 +1,8 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['container'],
-  session: null,
-
-  userModel: computed('userManager.user', function() {
-    return this.get('userManager.user');
-  }),
-
   organization: {},
 
   isLoading: false,
