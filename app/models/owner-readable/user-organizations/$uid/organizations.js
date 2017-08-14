2ac2f05797bcff5dabac63005c296ae38002751b
Josh Worden <solocommand@gmail.com>
2017-08-14 14:32:41 -0500

    polyfill for assign

--- app/models/owner-readable/user-organizations/$uid/organizations.js
+++ app/models/owner-readable/user-organizations/$uid/organizations.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-const { inject: { service }, RSVP: { Promise } } = Ember;
+const { inject: { service }, RSVP: { Promise }, assign } = Ember;
 
 export default Model.extend({
   userManager: service(),
@@ -23,7 +23,7 @@ export default Model.extend({
     const oid = this.get('id');
 
     const writeTo = this.get('fb').setToOrgOwnerWriteableQueue.bind(this.get('fb'));
-    const payload = Object.assign({}, this.getProperties('name', 'photoURL'), { updatedAt: this.get('fb').getTimestamp() });
+    const payload = assign({}, this.getProperties('name', 'photoURL'), { updatedAt: this.get('fb').getTimestamp() });
     return Promise.resolve()
       .then(() => writeTo('push', 'org-update', uid, oid, payload))
     ;
